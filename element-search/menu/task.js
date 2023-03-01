926b8e1e3048f1d401d64ff0f25c66c5dac2bd71
wee-owl <kem-cv@yandex.ru>
2023-03-01 11:27:17 +0300

    feat: add double menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,31 @@
-// массив из списка всех li в menu_main
-const menuMain = document.querySelector('.menu_main');
-const menuMainArray = Array.from(menuMain.children);
+// список всех menu_main
+const menuMain = document.querySelectorAll('.menu_main');
 
-// массив из списка всех menu_sub в menu_main
-const menuSub = menuMain.querySelectorAll('.menu_sub');
-const menuSubArray = Array.from(menuSub);
+const menuControl = (item) => {
+  // массив из списка всех li в menu_main
+  const menuMainArray = Array.from(item.children);
 
-// массив из menu__item, которые имеют menu_sub
-const menuItemSub = menuMainArray.filter(el => el.querySelector('.menu_sub'));
+  // массив из списка всех menu_sub в menu_main
+  const menuSub = item.querySelectorAll('.menu_sub');
+  const menuSubArray = Array.from(menuSub);
 
+  // массив из menu__item, которые имеют menu_sub
+  const menuItemSub = menuMainArray.filter(el => el.querySelector('.menu_sub'));
 
-menuItemSub.forEach((item, i) => {
-  item.addEventListener('click', (event) => {
-    event.preventDefault();
-    menuSubArray.forEach((sub, j) => {
-      if (i === j) {
-        sub.classList.toggle('menu_active');
-      } else {
-        sub.classList.remove('menu_active');
-      }
+  menuItemSub.forEach((item, i) => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      menuSubArray.forEach((sub, j) => {
+        if (i === j) {
+          sub.classList.toggle('menu_active');
+        } else {
+          sub.classList.remove('menu_active');
+        }
+      });
     });
   });
+};
+
+menuMain.forEach(menu => {
+  menuControl(menu);
 });
