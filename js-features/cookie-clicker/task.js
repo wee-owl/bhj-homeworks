b6036c9748c87910724f849076503743f4305950
wee-owl <kem-cv@yandex.ru>
2023-03-03 12:19:55 +0300

    refactor: ternary operator instead of else-if in cookie-clicker solution

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,7 +3,6 @@ const speedStatus = document.createElement('div');
 speedStatus.classList.add('speed__status');
 speedStatus.innerHTML = `Скорость клика: <span id="speed__counter">0</span>`;
 clickerStatus.insertAdjacentElement('afterend', speedStatus);
-
 const clickerCounter = document.getElementById('clicker__counter');
 const speedCounter = document.getElementById('speed__counter');
 const cookie = document.querySelector('.clicker__cookie');
@@ -19,11 +18,10 @@ const countClick = () => {
 
 
 const changeImgSize = () => {
-  if (cookie.clientWidth === 200) {
-    cookie.width = "220";
-  } else {
-    cookie.width = "200";
-  }
+  // вариант преподавателя
+  cookie.width = ++clickerCounter.textContent % 2 ? 250 : 200;
+  // мой вариант
+  // cookie.width = cookie.clientWidth === 200 ? 220 : 200;
 };
 
 
