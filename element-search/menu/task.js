cb9ae6a21ae756cc4c7580b3b8b0b3c3cb1f5438
wee-owl <kem-cv@yandex.ru>
2023-03-03 15:21:25 +0300

    feat: add close on click outside for menu solution

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,26 +1,23 @@
-// список всех menu_main
 const menuMain = document.querySelectorAll('.menu_main');
 
 const menuControl = (item) => {
-  // массив из списка всех li в menu_main
   const menuMainArray = Array.from(item.children);
-
-  // массив из списка всех menu_sub в menu_main
   const menuSub = item.querySelectorAll('.menu_sub');
   const menuSubArray = Array.from(menuSub);
-
-  // массив из menu__item, которые имеют menu_sub
   const menuItemSub = menuMainArray.filter(el => el.querySelector('.menu_sub'));
 
   menuItemSub.forEach((item, i) => {
-    item.addEventListener('click', (event) => {
-      event.preventDefault();
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
       menuSubArray.forEach((sub, j) => {
         if (i === j) {
           sub.classList.toggle('menu_active');
         } else {
           sub.classList.remove('menu_active');
         }
+        window.addEventListener('click', (e) => {
+          if (!e.target.closest('.menu__item')) sub.classList.remove('menu_active');
+        });
       });
     });
   });
