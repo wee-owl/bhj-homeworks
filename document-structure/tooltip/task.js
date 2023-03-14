0a4603853a0a81a15827f5e4dfdbcd54840420c9
wee-owl <kem-cv@yandex.ru>
2023-03-14 13:27:50 +0300

    feat: add data-attribute to tooltip solution

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,11 +4,23 @@ const createTooltip = (tooltip) => {
   let tooltipElem = document.createElement('div');
   tooltipElem.classList.add('tooltip');
   tooltipElem.classList.add('tooltip_active');
+  tooltipElem.setAttribute('data-position', 'bottom');   // задать значение атрибута для проверки расположения подсказки
   tooltipElem.innerHTML = tooltip.title ? `${tooltip.title}` : 'Здесь должна быть подсказка';
   tooltip.append(tooltipElem);
-  tooltipElem.style.left = tooltip.offsetLeft + 'px';
-  tooltipElem.style.top = (tooltip.offsetTop + tooltip.offsetHeight + 5) + 'px';
-  //tooltipElem.setAttribute('data-position', '');
+
+  if (tooltipElem.dataset.position === 'top') {
+    tooltipElem.style.top = (tooltip.offsetTop - tooltipElem.offsetHeight) + 'px';
+    tooltipElem.style.left = tooltip.offsetLeft + 'px';
+  }
+  if (tooltipElem.dataset.position === 'right') {
+    tooltipElem.style.top = (tooltipElem.offsetTop - tooltipElem.offsetHeight) + 'px';
+    tooltipElem.style.left = (tooltip.offsetLeft + tooltip.offsetWidth) + 'px';
+  }
+  if (tooltipElem.dataset.position === 'bottom') tooltipElem.style.left = tooltip.offsetLeft + 'px';
+  if (tooltipElem.dataset.position === 'left') {
+    tooltipElem.style.top = (tooltipElem.offsetTop - tooltipElem.offsetHeight) + 'px';
+    tooltipElem.style.left = (tooltip.offsetLeft - tooltipElem.offsetWidth) + 'px';
+  }
 };
 
 tooltips.forEach(tooltip => {
