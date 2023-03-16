0c352907cb0285d79c56562d49ad581d1868ec6f
wee-owl <kem-cv@yandex.ru>
2023-03-16 10:39:28 +0300

    feat: add property indeterminate to interests task solution

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,8 +1,9 @@
 const parentInterests = document.querySelector('.interests_main ul').children;
 
 [...parentInterests].forEach(item => {
+  const childInterests = item.lastElementChild.children;
+
   item.firstElementChild.addEventListener('click', (e) => {
-    const childInterests = item.lastElementChild.children;
     [...childInterests].forEach(el => {
       if (item.firstElementChild.children[0].checked) {
         el.firstElementChild.children[0].checked = true;
@@ -10,5 +11,22 @@ const parentInterests = document.querySelector('.interests_main ul').children;
         el.firstElementChild.children[0].checked = false;
       }
     });
+  });
+
+  item.lastElementChild.addEventListener('click', (e) => {
+    if (e.target.closest('label')) {
+      if ([...childInterests].some(el => !el.firstElementChild.children[0].checked)) {
+        item.firstElementChild.children[0].checked = false;
+        item.firstElementChild.children[0].indeterminate = true;
+      }
+      if ([...childInterests].every(el => !el.firstElementChild.children[0].checked)) {
+        item.firstElementChild.children[0].checked = false;
+        item.firstElementChild.children[0].indeterminate = false;
+      }
+      if ([...childInterests].every(el => el.firstElementChild.children[0].checked)) {
+        item.firstElementChild.children[0].checked = true;
+        item.firstElementChild.children[0].indeterminate = false;
+      }
+    };
   });
 });
