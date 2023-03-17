92087e6359cc5af06a11b219092888de3aad877f
wee-owl <kem-cv@yandex.ru>
2023-03-17 11:34:14 +0300

    fix: change attribute position in tooltip task

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,43 +1,57 @@
 const tooltips = document.querySelectorAll('.has-tooltip');
 
-const createTooltip = (tooltip) => {
+
+const createTooltip = (tooltip, position) => {
   let tooltipElem = document.createElement('div');
   tooltipElem.classList.add('tooltip');
   tooltipElem.classList.add('tooltip_active');
-  tooltipElem.setAttribute('data-position', 'bottom');   // задать значение атрибута для проверки расположения подсказки
   tooltipElem.innerHTML = tooltip.title ? `${tooltip.title}` : 'Здесь должна быть подсказка';
-  tooltip.append(tooltipElem);
+  tooltip.after(tooltipElem);
 
-  if (tooltipElem.dataset.position === 'top') {
+  tooltip.dataset.position = position;
+  if (tooltip.dataset.position === 'top') {
     tooltipElem.style.top = (tooltip.offsetTop - tooltipElem.offsetHeight) + 'px';
     tooltipElem.style.left = tooltip.offsetLeft + 'px';
   }
-  if (tooltipElem.dataset.position === 'right') {
+  if (tooltip.dataset.position === 'right') {
     tooltipElem.style.top = (tooltipElem.offsetTop - tooltipElem.offsetHeight) + 'px';
     tooltipElem.style.left = (tooltip.offsetLeft + tooltip.offsetWidth) + 'px';
   }
-  if (tooltipElem.dataset.position === 'bottom') tooltipElem.style.left = tooltip.offsetLeft + 'px';
-  if (tooltipElem.dataset.position === 'left') {
+  if (tooltip.dataset.position === 'bottom') {
+    tooltipElem.style.left = tooltip.offsetLeft + 'px';
+  }
+  if (tooltip.dataset.position === 'left') {
     tooltipElem.style.top = (tooltipElem.offsetTop - tooltipElem.offsetHeight) + 'px';
     tooltipElem.style.left = (tooltip.offsetLeft - tooltipElem.offsetWidth) + 'px';
   }
 };
 
+
 tooltips.forEach(tooltip => {
+  tooltip.setAttribute('data-position', '');
+
   tooltip.addEventListener('click', (e) => {
     e.preventDefault();
-    if (document.querySelector('.tooltip')) {
-      document.querySelector('.tooltip').remove();
-      createTooltip(tooltip);
+    if (tooltip.nextElementSibling === null || 
+      !tooltip.nextElementSibling.className.includes('tooltip_active')) {
+
+        let position = 'bottom';
+        let coords = tooltip.getBoundingClientRect();
+        if (coords.left < coords.width) {    // ограничение слева
+          position = 'right';
+        }
+        if (coords.top < coords.height*2) {    // ограничение сверху
+          position = 'bottom';
+        } 
+        if (window.innerWidth - coords.right < coords.width) {    // ограничение справа
+          position = 'left';
+        } 
+        if (window.innerHeight - coords.top < coords.height*2) {    // ограничение снизу
+          position = 'top';
+        }
+        createTooltip(tooltip, position);
     } else {
-      createTooltip(tooltip);
+        tooltip.nextElementSibling.remove();
     }
-    document.addEventListener('click', (e) => {
-      if (document.querySelector('.tooltip') && 
-          !e.target.closest('.has-tooltip') && 
-          !e.target.closest('.tooltip')) {
-        document.querySelector('.tooltip').remove();
-      }
-    });
   });
 });
