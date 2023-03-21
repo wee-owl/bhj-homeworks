6e6ccbd786cd09b03efdb28385c91b1d2759559c
wee-owl <kem-cv@yandex.ru>
2023-03-21 18:41:45 +0300

    feat: complete solution to progressbar task

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,12 +1,15 @@
 const API_URL = 'https://students.netoservices.ru/nestjs-backend/upload';
 const form = document.getElementById('form');
 const progress = document.getElementById('progress');
+const file = document.getElementById('file');
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const formSent = new FormData();
   const xhr = new XMLHttpRequest();
+
+  if (file) formSent.append('UploadFile', file.files[0]);
 
   xhr.upload.addEventListener('progress', (e) => {
     const value = e.loaded / e.total;
